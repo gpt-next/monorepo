935469481009ffb990c40b48ca08c37c81361650
Chris Felix <chrisfel@ca.ibm.com>
2023-12-30 23:32:58 -0500

    format

--- @gpt-next/go/src/index.ts
+++ @gpt-next/go/src/index.ts
@@ -1,1 +1,15 @@
-export { createDependencies } from './go-package-graph';
+import { CreateDependencies, DependencyType } from '@nx/devkit';
+
+export const createDependencies: CreateDependencies = (opts, ctx) => {
+  var results = [
+    {
+      source: 'test',
+      target: 'some-target',
+      sourceFile: 'main.go',
+      type: DependencyType.implicit,
+    },
+  ];
+  throw new Error('test');
+
+  return results;
+};
