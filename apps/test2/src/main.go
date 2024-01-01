03d90bb2b790030effe96180fe7561a12048175c
Chris Felix <chrisfel@ca.ibm.com>
2024-01-01 16:16:10 -0500

    test

--- apps/test2/src/main.go
+++ apps/test2/src/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "github.com/rs/zerolog/log"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
 )
-func main(){
-    log.Info().Msg("Hello from Zerolog logger")
-    fmt.Printf("test2: Hello World!")
+
+func main() {
+	log.Info().Msg("Hello from Zerolog logger")
+	fmt.Printf("test2: Hello World")
 }
