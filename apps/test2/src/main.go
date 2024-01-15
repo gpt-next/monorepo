70ec31a316d3b5a597af4cad8e7177a0bf03fbfb
Chris Felix <chrisfel@ca.ibm.com>
2024-01-15 12:40:24 -0500

    fake server

--- apps/test2/src/main.go
+++ apps/test2/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -9,4 +10,5 @@ import (
 func main() {
 	log.Info().Msg("Hello from Zerolog logger")
 	fmt.Printf("test2: Hello World!! new v1.0.0-rc2")
+	time.Sleep(24 * time.Hour)
 }
