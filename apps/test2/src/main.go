3db89b5c4bdf6c7d2a9ee8ebd8af81800ff639f4
Chris Felix <chrisfel@ca.ibm.com>
2024-01-15 16:40:27 -0500

    test

--- apps/test2/src/main.go
+++ apps/test2/src/main.go
@@ -8,6 +8,6 @@ import (
 
 func main() {
 	log.Info().Msg("Hello from Zerolog logger")
-	log.Info().Msg("test2: Hello World!!!!!")
+	log.Info().Msg("test2: Hello World TEST")
 	time.Sleep(24 * time.Hour)
 }
