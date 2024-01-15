5cbed4440e66df11b48c64bb6a5e9a1ca6ea75b2
Chris Felix <chrisfel@ca.ibm.com>
2024-01-15 16:03:03 -0500

    test

--- apps/test/src/main.go
+++ apps/test/src/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	log.Info().Msg("Hello from Zerolog logger")
-	log.Info().Msg("test: Hello Chris!")
+	log.Info().Msg("test: Hello Chris!!")
 	model := model.Model{
 		Name: "test",
 	}
